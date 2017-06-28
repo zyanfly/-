fc6ee1b3086a7e61f05809473b1e090f0c9f6370
zyanfly <zyanfly1832@163.com>
2017-06-28 10:13:09 +0800

    Update javascrip.js

--- javascrip.js
+++ javascrip.js
@@ -1,11 +1,11 @@
 //用原生代码写事件监听
 var ul = document.getElementById('list');
-ul.addEventListener('click',function (e){
+ul.addEventListener('click', function(e) {
     //如果点击对象是a标签，需要e.preventDefault();阻止默认跳转行为
     var ele = e.target;
-    if(ele.className === 'active'){
+    if(ele.className === 'active') {
         ele.className = '';
-    }else if(!ele.className){
-        ele.className='active';
+    }else if(!ele.className) {
+        ele.className = 'active';
     }
 })
